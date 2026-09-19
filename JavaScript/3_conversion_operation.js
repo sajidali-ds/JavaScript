628a68bdbeeeb781a88383efb01e1a346764840c
Sajid Ali <sajidalidatascience@gmail.com>
2026-09-19 09:08:23 +0000

    operation in js

--- JavaScript/3_conversion_operation.js
+++ JavaScript/3_conversion_operation.js
@@ -45,3 +45,40 @@
 // let stringNumber=String(someNumber)
 // console.log(stringNumber) =>33
 // console.log(typeof(stringNumber)) => String
+
+//*** Operation*******//
+
+// let value=3
+// let negValue= -value
+// console.log(negValue)
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3); =>8
+// console.log(2/3); =>0.666
+// console.log(2%3); =>2
+
+// let str1="sajid";
+// let str2="ali";
+// let str3=str1 + str2;
+// console.log(str3); =>sajidali
+
+// console.log("1" + 2); =>12
+// console.log(1+"2");  =>12
+
+//console.log("1"+2+2) =>122
+// console.log(1+2+"2") =>32
+
+// console.log(true); =>true
+// console.log(+true); =>1
+
+// console.log(+""); =>0
+
+// let num1,num2,num3
+
+// num1=num2=num3=2+2 // wrong readibility
+
+// let gamecounter=100
+// gamecounter++ ya ++gamecounter same output 101
+// console.log(gamecounter); =>101 
