c41dca618ba221cb6f7e3c119c06a8caa0bcd8fa
Sajid Ali <sajidalidatascience@gmail.com>
2026-09-19 07:55:16 +0000

    Datatypes in JavaScripts

---
+++ JavaScript/2_datatypes.js
@@ -0,0 +1,25 @@
+// alert("hello")  // we are using node.js not browser so error aayega
+
+//console.log(3+3); console.log("Sajid") // readablity khrb h so new line me likhenge
+
+console.log(3
+    +3) // readability should be high .here low
+console.log("Sajid")
+
+let name ="sajid" // string datatype
+let age=18 // number datatype
+let isloggedIn = false // boolean datatype
+let state; // undefined datatype
+// number => 2 to power 53 (range)
+// bigint 
+// string => " "
+// boolean => true/false
+// null => standalone value
+// undefined =>
+// symbol => for finding uniqueness
+
+// object
+
+console.log(typeof "hitesh") //string
+console.log(typeof null) // object
+console.log(typeof undefined); // undefined
