aa56ffbdffa9530f723831dd5073e45000aaa01e
Sajid Ali <sajidalidatascience@gmail.com>
2026-09-19 08:30:17 +0000

    Conversion of datatype

---
+++ JavaScript/3_conversion_operation.js
@@ -0,0 +1,47 @@
+// let score="33"
+// console.log(typeof score); =>String
+// console.log(typeof (score)); => String
+
+// let valueInNumber=Number(score);
+// console.log(typeof valueInNumber);
+// console.log(score);
+
+// let diff=null;
+// let NUM_VAL=Number(diff)
+// console.log(typeof diff)  =>0
+//console.log(NUM_VAL)  =>Number
+
+// let diff1=undefined;
+// let NUM_VAL1=Number(diff1)
+// console.log(typeof diff1)  => Nan
+// console.log(NUM_VAL1)  => Number
+
+// "33" =>33;
+//"33abc" =>Nan
+//true=> 1 &false =>0;
+
+// let isLoggedIn=1
+// let booleanIsLoggedIn=Boolean(isLoggedIn)
+// console.log(isLoggedIn) => 1
+// console.log(booleanIsLoggedIn) =>True
+
+// let isLoggedIn=""  
+// let booleanIsLoggedIn=Boolean(isLoggedIn)
+// console.log(isLoggedIn) =>""
+// console.log(booleanIsLoggedIn) =>false
+
+// let isLoggedIn="Sajid"  
+// let booleanIsLoggedIn=Boolean(isLoggedIn)
+// console.log(isLoggedIn) =>Sajid
+// console.log(booleanIsLoggedIn) =>true
+
+// Restriction
+
+//1 =>true , 0=>false;
+//"" =>false
+//"sajid" =>true
+
+// let someNumber=33
+// let stringNumber=String(someNumber)
+// console.log(stringNumber) =>33
+// console.log(typeof(stringNumber)) => String
