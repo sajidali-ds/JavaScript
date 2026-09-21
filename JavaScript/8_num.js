4ca62bf4ddf084701a79a987524680947ac52815
Sajid Ali <sajidalidatascience@gmail.com>
2026-09-21 18:40:13 +0000

    Number and Math in JS

---
+++ JavaScript/8_num.js
@@ -0,0 +1,37 @@
+// const score=400
+// console.log(score);
+// const balance=new Number(100);
+// console.log(balance);// datatype number fixed
+
+// console.log(balance.toString().length);//3
+// console.log(balance.toFixed(2)); //100.00
+
+// const otherNumber=123.8966
+// console.log(otherNumber.toPrecision(3));//124
+// console.log(otherNumber.toPrecision(4)) //123.9
+
+// const hundreds=1000000
+// console.log(hundreds.toLocaleString('en-IN'));//10,00,000
+
+//********Math*******/
+
+// console.log(Math);
+// console.log(Math.abs(-4)) //4
+// console.log(Math.round(4.3)); //4
+// console.log(Math.ceil(4.2)) // 5
+// console.log(Math.floor(4.9)) //4
+// console.log(Math.min(2,3,1,6,9)) //1
+// console.log(Math.max(2,3,1,6,9)) //9
+
+console.log(Math.random()) // value between 0 to 1 always
+console.log(Math.random()*10+1); // val greater than 1 
+console.log((Math.random()*10)+1);// between 1 to 10
+
+const min=10
+const max=20
+console.log(Math.floor(Math.random() * (max-min +1))+min)// value btw 10 to 20;
+
+
+
+
+
